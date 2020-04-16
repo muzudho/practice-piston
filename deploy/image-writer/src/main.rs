28c0741bad223f73d3e16956aa591f417907ba87
むずでょ <muzudho1@gmail.com>
2020-04-16 20:23:26 +0900

    write image.

--- deploy/image-writer/src/main.rs
+++ deploy/image-writer/src/main.rs
@@ -47,8 +47,45 @@ fn main() {
         )
         .unwrap();
     }
+
+    // サイズ指定で☆（＾～＾）
+    {
+        let frame = Frame::new(32, 64);
+        image::save_buffer(
+            &Path::new("output/32x64.png"),
+            &frame.to_vec(),
+            frame.width,
+            frame.height,
+            ColorType::Rgba8,
+        )
+        .unwrap();
+    }
 }
 
+struct Frame {
+    pub dots: Vec<Dot>,
+    pub width: u32,
+    pub height: u32,
+}
+impl Frame {
+    pub fn new(width: u32, height: u32) -> Self {
+        Frame {
+            dots: vec![Dot::default(); (width * height) as usize],
+            width: width,
+            height: height,
+        }
+    }
+
+    pub fn to_vec(&self) -> Vec<u8> {
+        let mut vec: Vec<u8> = Vec::new();
+        for dot in &self.dots {
+            vec.extend_from_slice(&dot.array());
+        }
+        vec
+    }
+}
+
+#[derive(Clone)]
 struct Dot {
     pub r: u8,
     pub g: u8,
@@ -58,5 +95,15 @@ struct Dot {
 impl Dot {
     pub fn array(&self) -> [u8; 4] {
         [self.r, self.g, self.b, self.a]
+    }
+}
+impl Default for Dot {
+    fn default() -> Self {
+        Dot {
+            r: 0,
+            g: 128,
+            b: 128,
+            a: 255,
+        }
     }
 }
